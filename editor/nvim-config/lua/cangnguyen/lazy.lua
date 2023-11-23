37c2411a68e3e62d7da21bfb4ee05c5f229812dd
cang.nguyent2 <cang.nguyent2@homecredit.vn>
2023-11-23 17:56:11 +0700

    Add mason-lspconfig

--- editor/nvim-config/lua/cangnguyen/lazy.lua
+++ editor/nvim-config/lua/cangnguyen/lazy.lua
@@ -16,7 +16,9 @@ require('lazy').setup({
 	{'hrsh7th/cmp-nvim-lsp'},
 	{'hrsh7th/nvim-cmp'},
 	{'L3MON4D3/LuaSnip'},
+
     {'williamboman/mason.nvim'},
+    {"williamboman/mason-lspconfig.nvim"},
 
      {
         "windwp/nvim-autopairs",
