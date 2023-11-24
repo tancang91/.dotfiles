c0576fb142fce9dbc275937738da10a79abbd208
cang.nguyent2 <cang.nguyent2@homecredit.vn>
2023-11-24 17:11:06 +0700

    Update neovim

--- editor/nvim-config/after/plugin/telescope.lua
+++ editor/nvim-config/after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local builtin = require('telescope.builtin')
+local actions = require("telescope.actions")
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
@@ -8,10 +9,16 @@ end)
 require('telescope').setup {
     defaults = {
         file_ignore_patterns = {
+            "__pycache__/",
             "node_modules/",
             "bin/",
             "build/",
-            "target/"
+            "target/",
+        },
+        mappings = {
+            i = {
+                ["<esc>"] = actions.close
+            }
         }
     }
 }
